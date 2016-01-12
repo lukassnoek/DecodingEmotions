320d9b6b5d00c79cf0af6925f9189206c3c5ec49
Lukas Snoek <lukassnoek@gmail.com>
2016-01-12 18:49:50 +0100

    Implement Fsl2mvp() instead of glm2mvpa

--- Analyses/transform_glm2mvpa.py
+++ Analyses/transform_glm2mvpa.py
@@ -9,15 +9,16 @@ import numpy as np
 import glob
 from os.path import join as opj
 sys.path.append('/home/lukas/Dropbox/PhD_projects/scikit_BOLD/')
-from data2mvpa import glm2mvpa, load_mvp_object
-import matplotlib.pyplot as plt
-
-mask = '/home/lukas/Dropbox/PhD_projects/DynamicAffect_Multiscale/ROIs/GrayMatter.nii.gz'
+from data2mvpa import Fsl2mvp, DataHandler
 
 self_dirs = glob.glob('/home/lukas/DecodingEmotions/Self/*/*.feat')
-_ = [shutil.rmtree(d) for d in [os.path.dirname(tdir) + '/mvp_data' for tdir in self_dirs] if os.path.exists(d)]
-_ = [glm2mvpa(self_dir, mask=mask, beta2tstat=True) for self_dir in self_dirs]
 
-other_dirs = glob.glob('/home/lukas/DecodingEmotions/Other/*/*.feat')
-_ = [shutil.rmtree(d) for d in [os.path.dirname(tdir) + '/mvp_data' for tdir in other_dirs] if os.path.exists(d)]
-_ = [glm2mvpa(other_dir, remove_class=['cue'], mask=mask, beta2tstat=True) for other_dir in other_dirs]
+"""
+for dr in self_dirs:
+    fsl2mvp = Fsl2mvp(dr, mask=None, mask_threshold=0, remove_class=[],
+                 ref_space='epi', beta2tstat=True, cleanup=1)
+    fsl2mvp.transform().merge_runs()
+"""
+
+mvp_dirs = glob.glob('/home/lukas/DecodingEmotions/Self/*/mvp_data')
+test = DataHandler(mvp_dirs[0], identifier='merged', shape='2D').load()
